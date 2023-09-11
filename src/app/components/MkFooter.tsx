f7afcf8528dd10f5ada5f0899f5af216a6b2207f
Task Nagashige <task.19.xx@gmail.com>
2023-09-11 23:12:56 +0900

    :zap: コンポーネント群を追加

---
+++ src/app/components/MkFooter.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export const MkFooter = () => {
+  console.log('MkFooter')
+
+  return (
+    <footer>
+      <p>©︎ 2023 鹿児島.mk</p>
+    </footer>
+  )
+}
---
+++ src/app/components/MkHeader.tsx
@@ -0,0 +1,9 @@
+export const MkHeader = () => {
+  console.log('MkHeader')
+
+  return (
+    <header>
+      <h1>Next.js ミートアップ【鹿児島.mk】 サンプルブログ</h1>
+    </header>
+  )
+}
