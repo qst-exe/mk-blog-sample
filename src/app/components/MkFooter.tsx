886d9a7b0a9d3d80d14f5087282b28224d00d02a
Task Nagashige <task.19.xx@gmail.com>
2023-09-15 15:12:40 +0900

    :lipstick: ヘッダーデザインの調整

--- src/app/components/MkFooter.tsx
+++ src/app/components/MkFooter.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import styles from "@/app/page.module.css";
+
 export const MkFooter = () => {
   console.log('MkFooter')
 
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>©︎ 2023 鹿児島.mk</p>
     </footer>
   )
--- src/app/components/MkHeader.tsx
+++ src/app/components/MkHeader.tsx
@@ -1,9 +1,11 @@
+import styles from '@/app/page.module.css'
+
 export const MkHeader = () => {
   console.log('MkHeader')
 
   return (
-    <header>
-      <h1>Next.js ミートアップ【鹿児島.mk】 サンプルブログ</h1>
+    <header className={styles.header}>
+      <p>Next.js ミートアップ【鹿児島.mk】 サンプルブログ</p>
     </header>
   )
 }
